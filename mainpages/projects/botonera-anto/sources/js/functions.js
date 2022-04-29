af31884f02d3611016c5e13491ee9c6239333379
rhomaxgamer <101533949+rhomaxgamer@users.noreply.github.com>
2022-04-28 22:11:35 -0300

    remover function a

--- mainpages/projects/botonera-anto/sources/js/functions.js
+++ mainpages/projects/botonera-anto/sources/js/functions.js
@@ -5,10 +5,6 @@ function reproducir(audio){
     //sonido.playbackRate = 2; 
 }
 
-function mostrarMsj(){
-    alert('A')
-}
-
 function mensajeDs(){
     let elemento = document.getElementById('cws-img');
     elemento.addEventListener("mouseover", mostrarMsj, false)
